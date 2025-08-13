7476ad551ceed1e546441ffd7709386b41a179d9
Juan David Castro <juandavid4130@gmail.com>
2025-08-13 16:53:45 -0500

    Adding location condition on navbar mobile for page selected

--- src/components/navbar/NavbarMobile.jsx
+++ src/components/navbar/NavbarMobile.jsx
@@ -12,14 +12,14 @@ export default function NavbarMobile({ navigationItems, handleMobileMenu, mobile
 
                 <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-gradient-to-b from-[#78ecac] to-[#59d0e9] px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-[#033649]/10">
                     <div className="flex items-center justify-between">
-                    <a href="#" className="-m-1.5 p-1.5">
-                        <span className="sr-only">Your Company</span>
+                    <Link to="/" className="-m-1.5 p-1.5">
+                        <span className="sr-only">NuestraTierra logo</span>
                         <img
-                        alt=""
+                        alt="logo corpo"
                         src={nuestraTierraLogoMobile}
                         className="h-14 w-auto rounded-full"
                         />
-                    </a>
+                    </Link>
                     <button
                         type="button"
                         onClick={() => setMobileMenuOpen(false)}
@@ -32,16 +32,20 @@ export default function NavbarMobile({ navigationItems, handleMobileMenu, mobile
                     <div className="mt-6 flow-root">
                     <div className="text-center sm:text-left -my-6 divide-y divide-gray-500/10">
                         <div className="space-y-2 py-6">
-                        {navigationItems.map((item) => (
-                            <Link
-                            key={item.name}
-                            to={item.to}
-                            onClick={handleMobileMenu}
-                            className="-mx-3 block rounded-lg px-6 py-3 text-lg font-semibold text-[#033649] hover:text-[#336f85]"
-                            >
-                            {item.name}
-                            </Link>
-                        ))}
+                            {navigationItems.map(item => {
+                                const isActive = location.pathname === item.to;
+                                return (
+                                <Link
+                                    key={item.name}
+                                    to={item.to}
+                                    onClick={() => setMobileMenuOpen(false)}
+                                    className={`-mx-3 block rounded-lg px-6 py-3 text-sm font-semibold
+                                    ${isActive ? "text-[#458ea8]" : "text-blueText hover:text-[#458ea8]"}`}
+                                >
+                                    {item.name}
+                                </Link>
+                                );
+                            })}
                         </div>
                         <div className="py-6">
                         <Link
@@ -55,6 +59,7 @@ export default function NavbarMobile({ navigationItems, handleMobileMenu, mobile
                     </div>
                     </div>
                 </DialogPanel>
+
             </Dialog>
         </>
     )
