9bc69a9610a88a290a9d50485686266823e51aa3
Juan David Castro <juandavid4130@gmail.com>
2025-08-26 23:45:56 -0500

    Soem changes on Profile component and banner colors

--- src/components/home/PartnersBanner.jsx
+++ src/components/home/PartnersBanner.jsx
@@ -1,6 +1,6 @@
 export default function PartnersBanner() {
   return (
-    <div className="bg-[#033649] py-16 sm:pt-12">
+    <div className="bg-[#01222e] py-16 sm:pt-12">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <h2 className="text-center text-lg/8 font-semibold text-white">Con la confianza de algunos de los equipos más innovadores del país</h2>
         <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
